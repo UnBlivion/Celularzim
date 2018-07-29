02a7aaa1034ad177cfb6735a705d87708a28a312
dyuki <unblivion@gmail.com>
2018-07-28 23:19:38 -0300

    embelezando tela inicial

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let onoff = true;
 
 let horas = new Date().getHours();
 let minutos = new Date().getMinutes();
+let dias = new Date().getDate();
+let mes = new Date().getMonth();
+let ano = new Date().getFullYear();
+let todoosmeses = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+let meses = todoosmeses[mes];
+
+console.log(horas);
+console.log(minutos);
 
 botao.onclick = ligar;
 
@@ -17,5 +25,6 @@ function ligar() {
     }
 }
 
-document.querySelector('#relogio').innerHTML = `${horas}:${minutos}`;
-console.log(`${horas}:${minutos}`);
+document.querySelector('#horas').innerHTML = `${horas}:${minutos}`;
+
+document.querySelector('#data').innerHTML = `${dias}/${meses}/${ano}`;
