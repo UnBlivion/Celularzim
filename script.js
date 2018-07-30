facf8089c7b63471fb94c146e38418cba76f600c
dyuki <unblivion@gmail.com>
2018-07-29 22:25:10 -0300

    ciando nova tela e aplicando botao vaivolta

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 let botao = document.querySelector("#Home");
+let swipe = document.querySelector("footer");
 let onoff = true;
+let telaVaiVem = true;
+
+
+swipe.onclick = () => {
+    if (telaVaiVem == true) {
+        document.querySelector("#telasenha").style.transform = "translateX(0%) translateY(-100%)";
+        document.querySelector(".anime:nth-child(1)").style.transform = "rotate(180deg)";
+        document.querySelector(".anime:nth-child(2)").style.transform = "rotate(180deg)";
+        document.querySelector(".anime:nth-child(3)").style.transform = "rotate(180deg)";
+        document.querySelector(".anime:nth-child(4)").style.transform = "rotate(180deg)";
+        document.querySelector(".anime:nth-child(5)").style.transform = "rotate(180deg)";
+        document.querySelector(".anime:nth-child(6)").style.transform = "rotate(180deg)";
+        telaVaiVem = false;
+    }
+    else {
+        document.querySelector("#telasenha").style.transform = "translateX(-100%) translateY(-100%)";
+        document.querySelector(".anime:nth-child(1)").style.transform = "rotate(0deg)";
+        document.querySelector(".anime:nth-child(2)").style.transform = "rotate(0deg)";
+        document.querySelector(".anime:nth-child(3)").style.transform = "rotate(0deg)";
+        document.querySelector(".anime:nth-child(4)").style.transform = "rotate(0deg)";
+        document.querySelector(".anime:nth-child(5)").style.transform = "rotate(0deg)";
+        document.querySelector(".anime:nth-child(6)").style.transform = "rotate(0deg)";
+        telaVaiVem = true;
+    }
+}
+
 
 let horas = new Date().getHours();
 let minutos = new Date().getMinutes();
 let dias = new Date().getDate();
 let mes = new Date().getMonth();
 let ano = new Date().getFullYear();
-let todoosmeses = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+let todoosmeses = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 let meses = todoosmeses[mes];
 
 console.log(horas);
@@ -20,8 +47,18 @@ function ligar() {
         onoff = false;
     }
     else {
-        document.querySelector("#tela").style.transform = "scale(0.001)";
+        document.querySelector("#tela").style.transform = "scale(0.0005)";
         onoff = true;
+        if (telaVaiVem == false) {
+            document.querySelector("#telasenha").style.transform = "translateX(-100%) translateY(-100%)";
+            document.querySelector(".anime:nth-child(1)").style.transform = "rotate(0deg)";
+            document.querySelector(".anime:nth-child(2)").style.transform = "rotate(0deg)";
+            document.querySelector(".anime:nth-child(3)").style.transform = "rotate(0deg)";
+            document.querySelector(".anime:nth-child(4)").style.transform = "rotate(0deg)";
+            document.querySelector(".anime:nth-child(5)").style.transform = "rotate(0deg)";
+            document.querySelector(".anime:nth-child(6)").style.transform = "rotate(0deg)";
+            telaVaiVem = true;
+        }
     }
 }
 
